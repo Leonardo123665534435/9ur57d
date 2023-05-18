f561ffa47ec51ccb9c32143ab0dc43997770e72b
Leonardo123665534435 <120745840+Leonardo123665534435@users.noreply.github.com>
2023-05-18 19:39:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 9ur57d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/9ur57d-lewwqf)
---
+++ src/app/products.ts
@@ -0,0 +1,45 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'IPHONE',
+    price: 799,
+    description: 'Entrou no insta a bateria acaba',
+  },
+  {
+    id: 2,
+    name: 'SAMSUNG',
+    price: 699,
+    description: 'regaça mais funciona',
+  },
+  {
+    id: 3,
+    name: 'MOTOROLA',
+    price: 299,
+    description: 'Agite e acende',
+  },
+  {
+    id: 4,
+    name: 'XIAOMI',
+    price: 299,
+    description: 'A bateria é muito, o tempo de regarga é poco',
+  },
+  {
+    id: 5,
+    name: 'NOKIA',
+    price: 299,
+    description: 'Caiu e o mundo destruiu',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
